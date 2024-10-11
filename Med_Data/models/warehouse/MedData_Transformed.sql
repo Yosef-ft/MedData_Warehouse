09bec7a3bcf36497701c8ca69cda9db6dcfb0638
Yosef-ft <yosefetene02@gmail.com>
2024-10-11 18:51:53 +0300

    The cleaned data with new columns

--- Med_Data/models/warehouse/MedData_Transformed.sql
+++ Med_Data/models/warehouse/MedData_Transformed.sql
@@ -1,18 +1,31 @@
 {{config(materialized='table')}}
 
--- / Removing white spaces for the messages column
 
-WITH clean_data as (
-    SELECT 
-        TRIM(message) AS message_cleaned, *
-    FROM {{ref('MedData_cleaned')}}
+WITH Processed_data AS (
+    SELECT *
+    From {{ref('MedData_Processed')}}
+    WHERE channel_username = '@lobelia4cosmetics'
 )
 
 
-SELECT 
-    channel_username,
-    message_cleaned,
-    date,
-    media_path
+SELECT
+    SUBSTRING(message_cleaned 
+        FROM POSITION('^[a-zA-Z]+$' IN message_cleaned)
+        FOR POSITION('Price ' IN message_cleaned) - POSITION('^[a-zA-Z]+$' IN message_cleaned)) AS Product,
 
-FROM clean_data
+    SUBSTRING(message_cleaned 
+        FROM POSITION('Price ' IN message_cleaned) + LENGTH('Price ')
+        FOR POSITION(' birr' IN message_cleaned) - POSITION('Price ' IN message_cleaned)) AS Price,
+
+
+    SUBSTRING(message_cleaned 
+        FROM POSITION('Adress:- ' IN message_cleaned) + LENGTH('Adress:- ')
+        FOR POSITION('Open' IN message_cleaned) - POSITION('Adress:- ' IN message_cleaned)) AS Address,  
+
+
+    SUBSTRING(message_cleaned 
+        FROM POSITION('call ' IN message_cleaned) + LENGTH('call ')
+        FOR POSITION('Adress' IN message_cleaned) - POSITION('call ' IN message_cleaned)) AS Phone_number, *
+
+FROM Processed_data
+
