14c25e300173f8649e69e39c3ae8ba08b7734fa0
Yosef-ft <yosefetene02@gmail.com>
2024-10-15 11:02:46 +0300

    Endpoints to get items by id

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 import models, schemas
-from models import Telegram
+from models import Telegram, Products
 
 def get_items(model, db: Session, limit: int= 10):
 
@@ -13,3 +13,14 @@ def get_items(model, db: Session, limit: int= 10):
         return telegrams
 
     return db.query(model).limit(limit).all()
+
+
+def get_item(model, db: Session, item_id):
+    
+    if model == Telegram:
+        telegram = db.query(model).filter(model.ID == item_id).first()
+        telegram.Date = str(telegram.Date)
+
+        return telegram
+    
+    return db.query(model).filter(model.ID == item_id).first()
--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Below are 3 endpoints to get items
 @app.get('/products/', response_model=list[schemas.Product])
 def read_products(limit: int=10, db: Session = Depends(get_db)):
     products = crud.get_items(Products, db, limit=limit)
     return products
 
 @app.get('/telegram/', response_model= list[schemas.Telegram])
-def read_telegram_data(limit: int=10, db: Session = Depends(get_db)):
+def read_telegram_datas(limit: int=10, db: Session = Depends(get_db)):
     telegram = crud.get_items(Telegram, db, limit=limit)
     return telegram
 
@@ -22,3 +23,19 @@ def read_telegram_data(limit: int=10, db: Session = Depends(get_db)):
 def read_detected_images(limit: int =10, db: Session = Depends(get_db)):
     detected_images = crud.get_items(Detected_Images, db, limit=limit)
     return detected_images
+
+# Below are 3 endpoints to get items by id
+@app.get('/products/{item_id}', response_model=schemas.Product)
+def read_product(item_id: int, db: Session = Depends(get_db)):
+    product = crud.get_item(Products, db, item_id=item_id)
+    return product
+
+@app.get('/telegram/{item_id}', response_model=schemas.Telegram)
+def read_telegram_data(item_id: int, db:Session = Depends(get_db)):
+    telegram = crud.get_item(Telegram, db, item_id=item_id)
+    return telegram
+
+@app.get('/detected-images/{item_id}', response_model= schemas.Detected_Images)
+def read_detected_image(item_id: int, db: Session = Depends(get_db)):
+    image = crud.get_item(Detected_Images, db, item_id=item_id)
+    return image
